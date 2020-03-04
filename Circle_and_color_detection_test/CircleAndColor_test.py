8f9d88bba32c3d633993b6712366c817ded01b22
phillipbomholtz <phillip.bomholtz@gmail.com>
2020-03-04 11:16:33 +0100

    Fixed whitespacing

--- Circle_and_color_detection_test/CircleAndColor_test.py
+++ Circle_and_color_detection_test/CircleAndColor_test.py
@@ -24,37 +24,38 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080);
 
 #loop for displaying frames
 while(True):
-	ret, frame = cap.read() #read from camera and store
-	lul = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #greyscale
-	lul2 = cv2.blur(lul, (3, 3)) 
-	detected_circles = cv2.HoughCircles(lul2, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, param2 = 30, minRadius = 1, maxRadius = 40) #look for circles
+    ret, frame = cap.read() #read from camera and store
+    flem = cv2.imread('Test_field_foto.jpg',0)
+    lul = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #greyscale
+    lul2 = cv2.blur(lul, (3, 3)) 
+    detected_circles = cv2.HoughCircles(lul2, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, param2 = 30, minRadius = 1, maxRadius = 40) #look for circles
 
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   #converte rgb to hvs
-	lower = np.array([0,200,0])              #set lover accept boundury
-	upper = np.array([9,255,255])            #set higher accept boundury
-	mask = cv2.inRange(hsv, lower, upper)  #make mask of boundurys
-	res = cv2.bitwise_and(frame,frame, mask= mask) #get only desired color
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   #converte rgb to hvs
+    lower = np.array([0,200,0])              #set lover accept boundury
+    upper = np.array([9,255,255])            #set higher accept boundury
+    mask = cv2.inRange(hsv, lower, upper)  #make mask of boundurys
+    res = cv2.bitwise_and(frame,frame, mask= mask) #get only desired color
 
 
 
 	#if any circles
-	if detected_circles is not None:
-		detected_circles = np.uint16(np.around(detected_circles))
+    if detected_circles is not None:
+        detected_circles = np.uint16(np.around(detected_circles))
 
 		#for all cirlces
-		for pt in detected_circles[0, :]:
+        for pt in detected_circles[0, :]:
 
-			a, b, r = pt[0], pt[1], pt[2]  #circle coordinates
-			cv2.circle(frame, (a, b), r+10, (0, 255, 255), 2)  #draw area around with radius r
-			cv2.circle(frame, (a, b), 1, (100, 0, 100), 3)  #draw dot im middle
+            a, b, r = pt[0], pt[1], pt[2]  #circle coordinates
+            cv2.circle(frame, (a, b), r+10, (0, 255, 255), 2)  #draw area around with radius r
+            cv2.circle(frame, (a, b), 1, (100, 0, 100), 3)  #draw dot im middle
 
 
-	cv2.imshow('circles',frame)
-	cv2.imshow('blue color',res)
+    cv2.imshow('circles',frame)
+    cv2.imshow('blue color',res)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-	continue
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    continue
 
 cap.release()
 cv2.destroyAllWindows()
