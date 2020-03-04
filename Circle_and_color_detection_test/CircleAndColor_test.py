455809f30f1abcb98dba8e8143089c99d85b5d5f
phillipbomholtz <phillip.bomholtz@gmail.com>
2020-03-04 12:46:46 +0100

    Added boundury detection. only for picture currently!

--- Circle_and_color_detection_test/CircleAndColor_test.py
+++ Circle_and_color_detection_test/CircleAndColor_test.py
@@ -25,17 +25,27 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080);
 #loop for displaying frames
 while(True):
     ret, frame = cap.read() #read from camera and store
-    flem = cv2.imread('Test_field_foto.jpg',0)
+    flem = cv2.imread('Test_field_foto.jpg',1)
+
+
     lul = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #greyscale
     lul2 = cv2.blur(lul, (3, 3)) 
     detected_circles = cv2.HoughCircles(lul2, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, param2 = 30, minRadius = 1, maxRadius = 40) #look for circles
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   #converte rgb to hvs
-    lower = np.array([0,200,0])              #set lover accept boundury
-    upper = np.array([9,255,255])            #set higher accept boundury
-    mask = cv2.inRange(hsv, lower, upper)  #make mask of boundurys
-    res = cv2.bitwise_and(frame,frame, mask= mask) #get only desired color
+    hsv = cv2.cvtColor(flem, cv2.COLOR_BGR2HSV)   #converte rgb to hvs
+    lower = np.array([0,220,200])              #set lover accept boundury
+    upper = np.array([10,255,255])            #set higher accept boundury
+    mask = cv2.inRange(hsv, lower, upper)    #make mask of boundurys
+    res = cv2.bitwise_and(flem,flem, mask= mask) #get only desired color
 
+    rgb = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+    grayed = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+    contours,_ = cv2.findContours(grayed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(rgb, (x,y), (x+w,y+h), (0,255,0), 2)
 
 
 	#if any circles
@@ -50,8 +60,9 @@ while(True):
             cv2.circle(frame, (a, b), 1, (100, 0, 100), 3)  #draw dot im middle
 
 
-    cv2.imshow('circles',frame)
-    cv2.imshow('blue color',res)
+    #cv2.imshow('circles',frame)
+    cv2.imshow('cross',cv2.resize(rgb, (960, 540)))
+    #cv2.imshow('border',res)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
